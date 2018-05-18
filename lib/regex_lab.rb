c15ddbccbe676a499062f5cfe740812cda75bd6a
Precious Humphrey <phumphrey23@gmail.com>
2018-05-18 00:01:28 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -14,7 +14,10 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+ if text.match?(/^[A-Z].*\W$/)
+   return true
+ else false
+ end
 end
 
 def valid_phone_number?(phone)
