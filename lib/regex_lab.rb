f60c4828c55e79e151f9aec56fe9352ceab95785
Precious Humphrey <phumphrey23@gmail.com>
2018-05-18 00:16:31 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,28 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/\b[AEIOUaeiou][a-z]*\b/)
+    return true
+  else false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\b[Uu]n[a-z]*ing\b/)
 end
 
 def words_five_letters_long(text)
-
+ text.scan(/\b[a-zA-Z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/^[A-Z].*\W$/)
+   return true
+ else false
+  end
 end
 
 def valid_phone_number?(phone)
-
+ if phone.match(/\(?\d{3}[) ]?\d{3}[- ]?\d{4}$/)
+   return true
+ else false
+ end
 end
